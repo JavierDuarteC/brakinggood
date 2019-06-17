e6763e47f484415ff5416d4c45e3b9e8819166ca
Ricardo <ricardonior@gmail.com>
2019-06-17 11:03:36 -0500

    First

---
+++ Plataformas/Assets/CheckGrounded.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGrounded : MonoBehaviour
+{
+
+    private PlayerController player;
+    
+    void Start()
+    {
+        player = GetComponentInParent<PlayerController>();
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            player.grounded = true;    
+        }
+    }
+    
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            player.grounded = false;    
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
