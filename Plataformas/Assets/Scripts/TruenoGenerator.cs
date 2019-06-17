4b09b2249ebd3b2127f94965607b21b9169e9acf
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-06-17 15:06:54 -0500

    Mecánica: químicos cayendo - funcionando

---
+++ Plataformas/Assets/Scripts/TruenoGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruenoGenerator : MonoBehaviour
+{
+
+    public GameObject trueno;
+    private int i = 0;
+    private GameObject truenoNuevo;
+    private Camera nubeVoladora;
+
+    void Start()
+    {
+        nubeVoladora = Camera.main;
+    }
+    public void Update()
+    {
+
+        if (i > 150)
+        {
+            truenoNuevo = Instantiate(trueno);
+            i = 0;
+            // Debug.Log(nubeVoladora.rect.yMax);
+            // truenoNuevo.transform.position = new Vector3(nubeVoladora.transform.position.x-Random.Range(-7,7),nubeVoladora.transform.position.y+6, 0);
+            var randomPosition = new Vector3(Random.Range(-6f, 16f), 5.2f, 0);
+            truenoNuevo.transform.position = randomPosition;
+            print(randomPosition.x);
+        }
+        i++;
+
+    }
+    // Start is called before the first frame update
+
+}
---
+++ Plataformas/Assets/Scripts/TruenoScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TruenoScript : MonoBehaviour
+{
+    [Header("Fall Speed")]
+    public float speed = 3f;
+
+    // private GameObject[] targets;
+    // private GameObject target;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        // targets = GameObject.FindGameObjectsWithTag("Ground");
+        // target = targets[Random.Range(0, targets.Length)];
+        Destroy(this.gameObject, 15);
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetSceneAt(0).name);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, -10, 0), step);
+    }
+}
